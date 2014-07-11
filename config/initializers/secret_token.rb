be56f8a669955f9b4169657b18af97d5c5392744
Fan <fzheng3@wisc.edu>
2014-07-11 11:40:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rivulets::Application.config.secret_key_base = 'a5617507e39f83667f32bb3a4351735cc48471dc5025d10521b3156a4c9facb20b128752570d70f74d5c883759a919c96a45e511ede85291dcf27e7048593827'
