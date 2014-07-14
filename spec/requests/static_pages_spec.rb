6ab5a01c26b2d50a147d0af41858caeb1018ba5d
Fan <fzheng3@wisc.edu>
2014-07-13 19:23:31 -0500

    layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,8 +5,14 @@ describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Rivulets'" do
       
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Rivulets')
     end
   end
+  
+  # it "should have the right links on the layout" do
+  #   visit root_path
+  #   click_link "Rivulets"
+  #   page.should have_selector 'h1', text: 'Rivulets'
+  # end
 end
